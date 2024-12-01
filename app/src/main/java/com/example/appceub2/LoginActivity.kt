657198be7ed1f3c043503687a3ad69d2f5e45a4a
Daniel Ramos <meirelesramos@gmail.com>
2024-12-01 03:13:19 -0300

    versão final

---
+++ app/src/main/java/com/example/appceub2/LoginActivity.kt
@@ -0,0 +1,45 @@
+package com.example.appceub2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.appceub2.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var databaseHelper: DatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        databaseHelper = DatabaseHelper(this)
+
+        binding.loginButton.setOnClickListener {
+            val loginUsername = binding.loginUsername.text.toString()
+            val loginPassword = binding.loginPassword.text.toString()
+            loginDatabase(loginUsername, loginPassword)
+        }
+
+        binding.signupRedirect.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun loginDatabase(username: String, password: String) {
+        val userExists = databaseHelper.readUser(username, password)
+        if (userExists) {
+            Toast.makeText(this, "Logado com sucesso", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            Toast.makeText(this, "Falha no login", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
--- app/src/main/java/com/example/appceub2/MainActivity.kt
+++ app/src/main/java/com/example/appceub2/MainActivity.kt
@@ -1,39 +1,19 @@
 package com.example.appceub2
 
 import android.content.Intent
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.appceub2.databinding.ActivityMainBinding
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var db: NotesDatabaseHelper
-    private lateinit var notesAdapter: NotesAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        db = NotesDatabaseHelper(this)
-        notesAdapter = NotesAdapter(db.getAllNotes(), this)
-
-        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
-        binding.notesRecyclerView.adapter = notesAdapter
-
-        binding.addButton.setOnClickListener {
-            val intent = Intent(this, AddNoteActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        notesAdapter.refreshData(db.getAllNotes())
+        val intent = Intent(this, NoteMainActivity::class.java)
+        Toast.makeText(this, "Abrindo app de notas", Toast.LENGTH_SHORT).show()
+        startActivity(intent)
+        finish()
     }
 }
